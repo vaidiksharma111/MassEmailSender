8cba91ddc8c0760a668a1155f6f9ed3a49a6e37c
Vaidik Sharma <chrisharry1281@gmail.com>
2023-09-15 11:51:41 +0530

    sending first email

--- app.js
+++ app.js
@@ -3,33 +3,37 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
+const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
+
+const publicpath=path.join(__dirname , '/public');
+
 // Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(publicpath));
 
 // Routes
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html');
+  res.sendFile(publicpath +'/index.html');
 });
 
-app.post('/send-email', (req, res) => {
+  app.post('/send-email', (req, res) => {
   const { to, subject, message } = req.body;
 
   // Create a nodemailer transporter
   const transporter = nodemailer.createTransport({
     service: 'Gmail',
     auth: {
-      user: 'vaidiksharma111@gmail.com', // Your Gmail email address
-      pass: 'password', // Your Gmail password or app-specific password
-    },
+      user: 'rayush920@gmail.com', // Your Gmail email address
+      pass: 'wvbkmppafssdcpfz', // Your Gmail password or app-specific password
+    }
   });
 
   // Email data
   const mailOptions = {
-    from: 'vaidiksharma111@gmail.com', // Sender's email address
+    from: 'rayush920@gmail.com', // Sender's email address
     to: to, // Recipient's email address
     subject: subject, // Email subject
     text: message, // Email message
@@ -37,12 +41,13 @@ app.post('/send-email', (req, res) => {
 
   // Send the email
   transporter.sendMail(mailOptions, (error, info) => {
+    // res.send(publicpath);
     if (error) {
       console.log(error);
       res.send('Error sending email.');
     } else {
       console.log('Email sent: ' + info.response);
-      res.send('Email sent successfully!');
+      res.sendFile(publicpath +'/success.html');
     }
   });
 });
