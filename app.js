354ac02496ab99998291fc8827a6dcfe179e78ff
Ayush raj <iec2020115@iiita.ac.in>
2023-09-16 16:41:54 +0530

    file upload kr do

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 // app.js
-
+//passcode for my email login ---  wvbkmppafssdcpfz
 const express = require('express');
 const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
 const session=require('express-session');
 const app = express();
 const path = require('path');
+const xlsx=require('xlsx');
 const port = process.env.PORT || 3000;
 
 
@@ -14,6 +15,11 @@ const publicpath=path.join(__dirname , '/public');
 // Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(publicpath));
+
+//Configure multer Middleware: 
+const multer = require('multer');
+const upload = multer({ dest: 'home/' }); // Set the destination folder for uploaded files
+
 
 //Configure express-session
 app.use(session({
@@ -24,17 +30,56 @@ app.use(session({
 
 // Routes
 app.get('/', (req, res) => {
+//   const workbook = xlsx.readFile(path.join(publicpath, 'new.xlsx'));
+// const worksheet = workbook.Sheets[workbook.SheetNames[0]];
+
+// for (let i = 2; i <= 5; i++) {
+//   const id = worksheet[`A${i}`].v;
+//   const name = worksheet[`B${i}`].v;
+
+//   console.log({ id, name });
+// }
 
   res.sendFile(publicpath +'/login.html');
 });
-const password = process.env.myPassword;
-app.get('/home', (req, res) => {
-   
-  const {email, password} = req.body;
-  // req.session.user="ayush";
-  req.session.user_email= email;
-  req.session.app_password= password;
-  res.sendFile(publicpath +'/home.html');
+app.post('/file-submit', (req, res) => {
+  if (!req.file) {
+    return res.status(400).send('No file uploaded.');
+  }
+  else
+  {
+    return res.send('file uploaded.');
+  }
+
+    const workbook = xlsx.readFile(path.join(publicpath, 'new.xlsx'));
+  const worksheet = workbook.Sheets[workbook.SheetNames[0]];
+  
+  for (let i = 2; i <= 5; i++) {
+    const id = worksheet[`A${i}`].v;
+    const name = worksheet[`B${i}`].v;
+  
+    console.log({ id, name });
+  }
+  
+    // res.sendFile(publicpath +'/login.html');
+  });
+
+
+app.post('/home', (req, res) => {
+  // Process the hometed form data here
+ 
+  try {
+    const { email, password } = req.body;
+      req.session.user_email= email;
+      req.session.app_password= password;
+      // res.send(email);
+      res.sendFile(publicpath +'/home.html');
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).send('Internal Server Error');
+  }
+  // Perform server-side operations like authentication, data storage, etc.
+
 });
 
 
@@ -48,8 +93,8 @@ app.post('/send-email', async (req, res) => {
   const transporter = nodemailer.createTransport({
     service: 'Gmail',
     auth: {
-      user: 'rayush920@gmail.com', // Your Gmail email address
-      pass: 'wvbkmppafssdcpfz', // Your Gmail password or app-specific password
+      user: req.session.user_email, // Your Gmail email address
+      pass: req.session.app_password, // Your Gmail password or app-specific password
     }
   });
 
@@ -57,7 +102,7 @@ app.post('/send-email', async (req, res) => {
     for (const recipient of toAddresses) {
       // Email data
       const mailOptions = {
-        from: 'rayush920@gmail.com', // Sender's email address
+        from: req.session.user_email, // Sender's email address
         to: recipient, // Current recipient's email address
         subject: subject, // Email subject
         text: message, // Email message
@@ -68,7 +113,7 @@ app.post('/send-email', async (req, res) => {
       console.log(`Email sent to: ${recipient}`);
     }
     // res.send(req.session.user_email);
-    // res.sendFile(publicpath + '/success.html');
+    res.sendFile(publicpath + '/success.html');
   } catch (error) {
   
     console.error(error);
