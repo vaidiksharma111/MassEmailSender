8a0147397026b8fa47b9aaf79ff7cecb0fc1e5c9
Vaidik Sharma <chrisharry1281@gmail.com>
2023-09-25 12:27:14 +0530

    updating code

---
+++ app.js
@@ -0,0 +1,197 @@
+const express = require('express');
+const nodemailer = require('nodemailer');
+const multer = require('multer');
+const session = require('express-session');
+const app = express();
+const path = require('path');
+const port = process.env.PORT || 3000;
+const publicpath = path.join(__dirname, '/public');
+const bodyParser = require('body-parser');
+const xlsx = require('xlsx');
+const globalFoundWords = [];
+
+// Create a storage engine for multer
+// const storage = multer.memoryStorage();
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
+// Middleware
+app.use(express.static(publicpath));
+app.use(bodyParser.urlencoded({ extended: false }));
+//qjvl skyp msvt rqwd
+//Configure express-session
+app.use(session({
+  secret: '1234', // A secret key used for session data encryption
+  resave: false,             // Do not save session data if not modified
+  saveUninitialized: true,   // Save uninitialized sessions
+}));
+
+function processMessageContent(req, res, next) {
+  const message = req.body.message;
+
+  // Find words starting with "#<" and ending with ">#" with a maximum length of 15 characters
+  const foundWords = message.match(/#<([^<>]{1,15})>#/g);
+  const extractedWords = [];
+
+  if (foundWords) {
+    // Remove the "#<" and ">#" delimiters from found words and store them
+    extractedWords.push(...foundWords.map((word) => word.slice(2, -2)));
+
+    // Print the extracted words
+    console.log('Found words:', extractedWords);
+  } else {
+    console.log('No words matching the pattern found.');
+  }
+
+  // Store the found words in the global variable
+  globalFoundWords.push(...extractedWords);
+
+  // Continue to the next middleware or route handler
+  next();
+}
+
+
+function extractValuesInColumn(sheet, columnIndex) {
+  const rows = xlsx.utils.sheet_to_json(sheet, { header: 1 });
+  const valuesInColumn = [];
+
+  for (let i = 1; i < rows.length; i++) {
+    if (rows[i][columnIndex]) {
+      valuesInColumn.push(rows[i][columnIndex]);
+    }
+  }
+
+  return valuesInColumn;
+}
+
+function extractColumnNames(sheet) {
+  const headers = xlsx.utils.sheet_to_json(sheet, { header: 1 })[0];
+  return headers;
+}
+
+// Routes
+app.get('/', (req, res) => {
+  res.sendFile(publicpath + '/home.html');
+});
+
+
+app.post('/send-email', upload.single('file'), processMessageContent, async (req, res) => {
+  console.log('Global Found Words:', globalFoundWords);
+  try {
+    if (req.file) {
+      const workbook = xlsx.read(req.file.buffer, { type: 'buffer' });
+      const sheetName = workbook.SheetNames[0];
+      const sheet = workbook.Sheets[sheetName];
+      const columnNames = extractColumnNames(sheet);
+      console.log('Column Names:', columnNames);
+
+      const matchingWords = getMatchingWords(columnNames);
+
+      if (matchingWords.length > 0) {
+        console.log('Matching Words:', matchingWords);
+
+        const columnValues = getColumnValues(sheet, matchingWords);
+        const toAddresses = req.body.to.split(',').map(email => email.trim());
+
+        await sendEmailsWithReplacement(req, matchingWords, columnValues, toAddresses);
+
+        console.log('Size of the List:', matchingWords.length);
+      } else {
+        console.log('No matching words found between globalFoundWords and column names.');
+      }
+    }
+    else {
+      const toAddresses = req.body.to.split(',').map(email => email.trim());
+      await sendEmailsWithoutReplacement(req, toAddresses);
+    }
+
+    // Redirect to success.html after sending the email
+    res.redirect('/success.html');
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: 'Internal server error.' });
+  }
+});
+
+
+function getMatchingWords(columnNames) {
+  return globalFoundWords.filter(word =>
+    columnNames.some(column => column.toLowerCase() === word.toLowerCase())
+  );
+}
+
+function getColumnValues(sheet, matchingWords) {
+  const columnValues = {};
+  matchingWords.forEach(word => {
+    const columnIndex = extractColumnIndex(sheet, word);
+    if (columnIndex !== -1) {
+      const valuesInColumn = extractValuesInColumn(sheet, columnIndex);
+      console.log(`Values in Column "${word}":`, valuesInColumn);
+      columnValues[word] = valuesInColumn;
+    }
+  });
+  return columnValues;
+}
+
+function extractColumnIndex(sheet, columnName) {
+  const headers = xlsx.utils.sheet_to_json(sheet, { header: 1 })[0];
+  return headers.findIndex(header => header.toLowerCase() === columnName.toLowerCase());
+}
+
+async function sendEmailsWithReplacement(req, matchingWords, columnValues, toAddresses) {
+  for (let i = 0; i < toAddresses.length; i++) {
+    const recipient = toAddresses[i];
+    let messageContent = req.body.message;
+
+    for (const word of matchingWords) {
+      const values = columnValues[word];
+      if (values.length > i) {
+        const regex = new RegExp(`#<${word}>#`, 'g');
+        messageContent = messageContent.replace(regex, values[i]);
+      }
+    }
+
+    console.log('Message Content:');
+    console.log(messageContent);
+
+    await sendEmail(req, messageContent, recipient);
+  }
+}
+
+async function sendEmailsWithoutReplacement(req, toAddresses) {
+  for (let i = 0; i < toAddresses.length; i++) {
+    const recipient = toAddresses[i];
+    let messageContent = req.body.message;
+
+    console.log('Message Content:');
+    console.log(messageContent);
+
+    await sendEmail(req, messageContent, recipient);
+  }
+}
+
+async function sendEmail(req, messageContent, recipient) {
+  const mailOptions = {
+    from: req.session.user_email, // Sender's email address
+    to: recipient, // Current recipient's email address
+    subject: req.body.subject, // Email subject
+    text: messageContent, // Email message
+  };
+
+  const transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+      user: 'vaidiksharma111@gmail.com',
+      pass: 'qjvl skyp msvt rqwd',
+    }
+  });
+
+  await transporter.sendMail(mailOptions);
+  console.log(`Email sent to: ${recipient}`);
+}
+
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
---
+++ public/script.js
@@ -0,0 +1,51 @@
+// Function to read and print Excel data with an "Email" column
+function readAndPrintEmailColumn() {
+  const fileInput = document.getElementById('fileInput');
+  const emailInput = document.getElementById('to'); // Get the "receiver-email" input field
+
+  if (!fileInput.files[0]) {
+    alert('No file selected');
+    return;
+  }
+
+  const file = fileInput.files[0];
+  const reader = new FileReader();
+
+  reader.onload = function (e) {
+    const data = e.target.result;
+    const workbook = XLSX.read(data, { type: 'array' });
+    const sheetName = workbook.SheetNames[0]; // Assuming you want the first sheet
+    const sheet = workbook.Sheets[sheetName];
+
+    // Convert the sheet to an array of objects
+    const excelData = XLSX.utils.sheet_to_json(sheet, { header: 1 });
+
+    // Check if there is an "Email" column
+    const headerRow = excelData[0];
+    const emailColumnIndex = headerRow.indexOf('Email');
+
+    if (emailColumnIndex === -1) {
+      alert('No column with the name "Email" found');
+      return;
+    }
+
+    // Collect all email values in the "Email" column
+    const emailValues = [];
+    for (let i = 1; i < excelData.length; i++) {
+      const rowData = excelData[i];
+      if (rowData.length > emailColumnIndex) {
+        emailValues.push(rowData[emailColumnIndex]);
+      }
+    }
+
+    // Set the "receiver-email" input field value with comma-separated email values
+    emailInput.value = emailValues.join(', ');
+  };
+
+  reader.readAsArrayBuffer(file);
+}
+
+
+
+
+
